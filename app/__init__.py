44e57a70dd86c43a4b901ecc8d49a130ff38b8d6
Tatenda <31291528+rideam@users.noreply.github.com>
2023-02-21 14:21:01 +0200

    update project requirements and sql alchemy initialisation

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{db_path}'
 app.config["SQLALCHEMY_ECHO"] = False
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 from app import views
 from app import models
